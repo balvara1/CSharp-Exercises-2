9a1e810b369ae2b4a6849e237d0a6996ee6a338f
balvara1 <84111063+balvara1@users.noreply.github.com>
2022-02-09 22:23:16 -0500

    Add project files.

---
+++ Bernadette-Alvarado_Exercise01/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bernadette_Alvarado_Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create countries list
+            List<Country> Countries = Country.GetCountries();
+
+            Console.WriteLine("Countries List:\n");
+            foreach (var item in Countries)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("\n\n");
+
+            // 1.1 List the names of the countries in alphabetical order 
+
+            Console.WriteLine("1.1 List the names of the countries in alphabetical order:\n");
+            var countriesAsc = from country in Countries orderby country.Name select country;
+            foreach (var item in countriesAsc)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("\n\n");
+
+            // 1.2 List the names of the countries in descending order of number of resources
+
+            Console.WriteLine("1.2 List the names of the countries in descending order of number of resources:\n");
+            var countriesNoResources = from country in Countries orderby country.Resources.Count descending select country;
+            foreach (var item in countriesNoResources)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("\n\n");
+
+            // 1.3 List the names of the countries that shares a border with Argentina
+
+            Console.WriteLine("1.3 List the names of the countries that shares a border with Argentina:\n");
+            var countryBorderArgentina = from country in Countries where country.Borders.Contains("Argentina") select country;
+            foreach (var item in countryBorderArgentina)
+            {
+                Console.WriteLine($"{item.Name,-15}{string.Join(", ", item.Borders),-25}");
+            }
+
+            Console.WriteLine("\n\n");
+
+            // 1.4 List the names of the countries that has more than 10,000,000 population 
+            Console.WriteLine("1.4 List the names of the countries that has more than 10,000,000 population:\n");
+            var countryPopulation = from country in Countries where country.Population > 10000000 orderby country.Population ascending select country;
+            foreach (var item in countryPopulation)
+            {
+                Console.WriteLine($"{item.Name,-15}{string.Join(", ", item.Population),-25}");
+            }
+
+            Console.WriteLine("\n\n");
+
+            // 1.5 List the country with highest population 
+            Console.WriteLine("1.5 List the country with highest population:\n");
+            var countryHighPopulation = (from country in Countries orderby country.Population descending select country).First();
+
+            Console.WriteLine($"{countryHighPopulation.Name,-15}{string.Join(", ", countryHighPopulation.Population),-25}");
+
+            Console.WriteLine("\n\n");
+
+            // 1.6 List all the religion in south America in dictionary order
+            Console.WriteLine("1.6 List all the religion in south America in dictionary order:\n");
+            var countryReligion = (from country in Countries from religions in country.Religions orderby religions select religions).Distinct();
+            foreach (var item in countryReligion)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
---
+++ Bernadette-Alvarado_Exercise02/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bernadette_Alvarado_Exercise02
+{
+    public class Program
+    {
+        public static BooksEntities book = new BooksEntities();
+
+        static void Main(string[] args)
+        {
+
+            // 1. Get a list of all the titles and the authors who wrote them. Sort the results by title.
+            var bookSortTitle = from book in book.Titles
+                           from author in book.Authors
+                           orderby book.Title1
+                           select new { book.Title1, author.FirstName, author.LastName };
+
+            Console.WriteLine("Book List sorted by Title:\n");
+            foreach (var item in bookSortTitle)
+            {
+                Console.WriteLine($"{item.Title1,-60}{item.FirstName,-10}{item.LastName,-10}");
+            }
+
+            Console.WriteLine("\n\n");
+
+            // 2. Get a list of all the titles and the authors who wrote them. Sort the results by title. Each title sort the authors 
+            // alphabetically by last name, then first name
+
+            Console.WriteLine("Book List sorted by Title and Author:\n");
+            var bookSortAuthor = from book in book.Titles
+                                         from author in book.Authors
+                                         orderby book.Title1, author.LastName, author.FirstName
+                                         select new { author.FirstName, author.LastName, book.Title1 };
+
+            foreach (var item in bookSortAuthor)
+            {
+                Console.WriteLine($"{item.Title1,-60}{item.FirstName,-10}{item.LastName,-10}");
+            }
+
+            Console.WriteLine("\n\n");
+
+            // 3.  Get a list of all the authors grouped by title, sorted by title; for a given title sort the author names 
+            // alphabetically by last name then first name.
+
+            Console.WriteLine("Book List grouped by Title, sorted by Title and Author:\n");
+            var bookGroupTitleSortAuthor = from book in book.Titles
+                                           orderby book.Title1 ascending
+                                           select new
+                                           { book.Title1, Authors = from author in book.Authors
+                                                         orderby author.LastName, author.FirstName
+                                                         select author };
+
+            foreach (var item in bookGroupTitleSortAuthor)
+            {
+                Console.WriteLine($"{item.Title1,-60}");
+                foreach (var authorItem in item.Authors)
+                {
+                    Console.WriteLine($"{authorItem.FirstName,-5}{authorItem.LastName}");
+                }
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
